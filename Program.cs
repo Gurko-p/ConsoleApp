b4e5b28d7d6e6258173e226b034076aeb051ba96
Pavel <gurko-p@mail.ru>
2022-05-25 18:49:50 +0300

    successful version1

--- Program.cs
+++ Program.cs
@@ -39,6 +39,7 @@ namespace ConsoleApp1
         {
             List<Abiturient> ab = new List<Abiturient>
             {
+
                 new Abiturient { Id = 3, Result = 370, Specialities = new int[]{ 1, 2, 0 } }, // 1
                 new Abiturient { Id = 2, Result = 270, Specialities = new int[]{ 2, 1, 0 } }, // 2
                 new Abiturient { Id = 4, Result = 270, Specialities = new int[]{ 1, 2, 0 } }, // 1
@@ -49,11 +50,28 @@ namespace ConsoleApp1
                 new Abiturient { Id = 9, Result = 245, Specialities = new int[]{ 1, 3, 2 } }, // -
                 new Abiturient { Id = 1, Result = 230, Specialities = new int[]{ 2, 1, 0 } }, // 2
 
+                //SPec1 = 3 - 370, 4 - 270
+                //SPec2 = 1, 2
+
+
+                //new Abiturient { Id = 3, Result = 300, Specialities = new int[]{ 1, 3, 0 } }, // 1
+                //new Abiturient { Id = 6, Result = 270, Specialities = new int[]{ 1, 0, 0 } }, // -
+                //new Abiturient { Id = 2, Result = 250, Specialities = new int[]{ 2, 0, 0 } }, // 2
+                //new Abiturient { Id = 9, Result = 225, Specialities = new int[]{ 1, 2, 3 } }, // -
+                //new Abiturient { Id = 4, Result = 200, Specialities = new int[]{ 1, 2, 0 } }, // 1
+                //new Abiturient { Id = 8, Result = 180, Specialities = new int[]{ 2, 3, 0 } }, // -
+                //new Abiturient { Id = 10, Result = 150, Specialities = new int[]{ 1, 2, 3 } }, // -
                 
+                
+                
+                //new Abiturient { Id = 5, Result = 240, Specialities = new int[]{ 1, 2, 0 } }, // -
+                //new Abiturient { Id = 1, Result = 260, Specialities = new int[]{ 2, 3, 0 } }, // 2
             };
 
-            SPec1 = 3, 4
-            SPec2 = 1, 2
+            //SPec1 = 3 - 300, 6 - 270
+            //SPec2 = 2 - 250, 9 - 225
+            //SPec3 = 1, 2
+
             List<Abiturient> intermediate = new List<Abiturient>();
             List<Abiturient> failed = new List<Abiturient>();
             List<int> success = new List<int>();
@@ -74,8 +92,11 @@ namespace ConsoleApp1
                     {
                         if (abiturients["sp" + sp].Count < abiturients["sp" + sp].Capacity)
                         {
-                            abiturients["sp" + sp].Add(ab[j]);
-                            success.Add(ab[j].Id);
+                            if (!success.Contains(ab[j].Id))
+                            {
+                                abiturients["sp" + sp].Add(ab[j]);
+                                success.Add(ab[j].Id);
+                            }
                         }
                         else
                         {
@@ -88,27 +109,35 @@ namespace ConsoleApp1
                                 abiturients["sp" + sp].Add(ab[j]);
                                 success.Remove(a.Id);
                                 success.Add(ab[j].Id);
-                                //Console.WriteLine(ab[j].Id);
 
+                                //Console.WriteLine(ab[j].Id);
                             }
+
+                        }
+                        if (i + 1 >= ab[j].Specialities.Length && !success.Contains(ab[j].Id))
+                        {
+                            failed.Add(ab[j]);
                         }
                     }
                     else
                     {
-                        failed.Add(ab[j]);
+                        if (!success.Contains(ab[j].Id))
+                        {
+                            failed.Add(ab[j]);
+                        }
                     }
                 }
             }
             Console.WriteLine("Spec1");
             for (int i = 0; i < dict["sp1"].Count; i++)
             {
-                Console.WriteLine(dict["sp1"][i].Id);
+                Console.WriteLine(dict["sp1"][i].Id + " - " + dict["sp1"][i].Result);
             }
             Console.WriteLine(" ----------------------------- ");
             Console.WriteLine("Spec2");
             for (int i = 0; i < dict["sp2"].Count; i++)
             {
-                Console.WriteLine(dict["sp2"][i].Id);
+                Console.WriteLine(dict["sp2"][i].Id + " - " + dict["sp2"][i].Result);
             }
             Console.WriteLine(" ----------------------------- ");
             Console.WriteLine("Intermediate");
@@ -122,7 +151,7 @@ namespace ConsoleApp1
             Console.WriteLine("Spec3");
             for (int i = 0; i < dict["sp3"].Count; i++)
             {
-                Console.WriteLine(dict["sp3"][i].Id);
+                Console.WriteLine(dict["sp3"][i].Id + " - " + dict["sp3"][i].Result);
             }
 
 
@@ -133,6 +162,12 @@ namespace ConsoleApp1
                 Console.WriteLine(success[i]);
             }
 
+            Console.WriteLine(" ----------------------------- ");
+            Console.WriteLine("Failed");
+            for (int i = 0; i < failed.Count; i++)
+            {
+                Console.WriteLine(failed[i].Id);
+            }
 
 
             Console.ReadKey();
