497ba67cbec13dfb588eeb57595f6378a0760f71
Pavel <gurko-p@mail.ru>
2022-05-25 15:27:23 +0300

    successful version

--- Program.cs
+++ Program.cs
@@ -19,6 +19,19 @@ namespace ConsoleApp1
         {
             return Result.CompareTo(a.Result);
         }
+        public static bool Contains(Abiturient[] a, Abiturient b)
+        {
+            bool res = false;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i].Id == b.Id)
+                {
+                    res = true;
+                    break;
+                }
+            }
+            return res;
+        }
     }
     class Program
     {
@@ -26,169 +39,102 @@ namespace ConsoleApp1
         {
             List<Abiturient> ab = new List<Abiturient>
             {
-                new Abiturient { Id = 3, Result = 370, Specialities = new int[]{1, 2} }, // 1
-                new Abiturient { Id = 2, Result = 270, Specialities = new int[]{2, 1} }, // 2
-                new Abiturient { Id = 5, Result = 240, Specialities = new int[]{1, 2} }, // -
-                new Abiturient { Id = 4, Result = 270, Specialities = new int[]{1, 2} }, // 1
-                new Abiturient { Id = 1, Result = 230, Specialities = new int[]{2, 1} } // 2
+                new Abiturient { Id = 3, Result = 370, Specialities = new int[]{ 1, 2, 0 } }, // 1
+                new Abiturient { Id = 2, Result = 270, Specialities = new int[]{ 2, 1, 0 } }, // 2
+                new Abiturient { Id = 4, Result = 270, Specialities = new int[]{ 1, 2, 0 } }, // 1
+                new Abiturient { Id = 10, Result = 250, Specialities = new int[]{ 2, 1, 3 } }, // -
+                new Abiturient { Id = 5, Result = 240, Specialities = new int[]{ 1, 2, 0 } }, // -
+                new Abiturient { Id = 6, Result = 245, Specialities = new int[]{ 1, 2, 0 } }, // -
+                new Abiturient { Id = 8, Result = 245, Specialities = new int[]{ 3, 2, 0 } }, // -
+                new Abiturient { Id = 9, Result = 245, Specialities = new int[]{ 1, 3, 2 } }, // -
+                new Abiturient { Id = 1, Result = 230, Specialities = new int[]{ 2, 1, 0 } }, // 2
+
+                
             };
 
-            //List<Abiturient> abSuccess = new List<Abiturient>(4);
-
-            //for (int i = 0; i < ab.Count; i++)
-            //{
-            //    Console.WriteLine("Объем: " + abSuccess.Count);
-            //    if (abSuccess.Count < abSuccess.Capacity)
-            //    {
-            //        abSuccess.Add(ab[i]);
-            //    }
-            //    else
-            //    {
-            //        Abiturient abFailed = abSuccess.Where(a => a.Result <= abSuccess.Min(a => a.Result)).LastOrDefault();
-            //        Console.WriteLine("Min:  " + abFailed.Id);
-            //        abSuccess.Remove(abFailed);
-            //        abSuccess.Add(ab[i]);
-            //    }
-            //}
-
-            //for (int i = 0; i < abSuccess.Count; i++)
-            //{
-            //    Console.WriteLine(abSuccess[i].Id);
-            //}
+            SPec1 = 3, 4
+            SPec2 = 1, 2
+            List<Abiturient> intermediate = new List<Abiturient>();
+            List<Abiturient> failed = new List<Abiturient>();
+            List<int> success = new List<int>();
 
             List<Abiturient> sp1 = new List<Abiturient>(2);
             List<Abiturient> sp2 = new List<Abiturient>(2);
+            List<Abiturient> sp3 = new List<Abiturient>(2);
+            Dictionary<string, List<Abiturient>> dict = new Dictionary<string, List<Abiturient>>() { ["sp1"] = sp1, ["sp2"] = sp2, ["sp3"] = sp3 };
 
-            List<Abiturient> failedList = new List<Abiturient>();
 
-            Dictionary<string, List<Abiturient>> dict = new Dictionary<string, List<Abiturient>>();
-            dict["sp1"] = new List<Abiturient>();
-            dict["sp2"] = new List<Abiturient>();
-            int minValue = 0;
-            Abiturient minAb = null;
-            //minValue = ab.Min(a => a.Result);
-
-            //Console.WriteLine(minValue);
-
-            for (int i = 0; i < 2; i++)
+            for (int i = 0; i < 3; i++)
             {
-                //if (dict["sp" + (i + 1)].Count == 0)
-                //{
-                //    minValue = ab[0].Result;
-                //    minAb = ab[0];
-                //}
-                //else
-                //{
-                //    minValue = dict["sp" + (i + 1)].Min(a => a.Result);
-                //    minAb = ab.Where(a => a.Result == minValue).LastOrDefault();
-                //    Console.WriteLine("Min: " + minAb.Id);
-                //}
                 for (int j = 0; j < ab.Count; j++)
                 {
-                    if (dict["sp" + (i + 1)].Count < dict["sp" + (i + 1)].Capacity)
+                    int sp = ab[j].Specialities[i];
+                    Dictionary<string, List<Abiturient>> abiturients = dict;
+                    if (sp != 0)
                     {
-                        dict["sp" + (i + 1)].Add(ab[j]);
+                        if (abiturients["sp" + sp].Count < abiturients["sp" + sp].Capacity)
+                        {
+                            abiturients["sp" + sp].Add(ab[j]);
+                            success.Add(ab[j].Id);
+                        }
+                        else
+                        {
+                            int minResSp = abiturients["sp" + sp].Min(a => a.Result);
+                            if (ab[j].Result > minResSp && !success.Contains(ab[j].Id))
+                            {
+                                Abiturient a = abiturients["sp" + sp].Where(a => a.Result == minResSp).LastOrDefault();
+                                //Console.WriteLine("Ab: " + a.Id);
+                                abiturients["sp" + sp].Remove(a);
+                                abiturients["sp" + sp].Add(ab[j]);
+                                success.Remove(a.Id);
+                                success.Add(ab[j].Id);
+                                //Console.WriteLine(ab[j].Id);
+
+                            }
+                        }
                     }
                     else
                     {
-                        Abiturient abFailed = dict["sp" + (i + 1)].Where(a => a.Result <= dict["sp" + (i + 1)].Min(a => a.Result)).LastOrDefault();
-                        Console.WriteLine("Min:  " + abFailed.Id);
-                        dict["sp" + (i + 1)].Remove(abFailed);
-                        dict["sp" + (i + 1)].Add(ab[i]);
+                        failed.Add(ab[j]);
                     }
-
-
-
-
-
-
-                    //if (ab[j].Result < minValue && dict["sp" + (i + 1)].Count < dict["sp" + (i + 1)].Capacity)
-                    //{
-                    //    try
-                    //    {
-                    //        dict["sp" + (i + 1)].Add(ab[j]);
-                    //        Console.WriteLine(ab[j].Id);
-                    //    }
-                    //    catch
-                    //    {
-                    //        Console.WriteLine("Ошибка!");
-                    //    }
-                    //}
-                    //else
-                    //{
-                    //    dict["sp" + (i + 1)].Remove(minAb);
-                    //    failedList.Add(minAb);
-                    //    dict["sp" + (i + 1)].Add(ab[j]);
-                    //}
                 }
+            }
+            Console.WriteLine("Spec1");
+            for (int i = 0; i < dict["sp1"].Count; i++)
+            {
+                Console.WriteLine(dict["sp1"][i].Id);
+            }
+            Console.WriteLine(" ----------------------------- ");
+            Console.WriteLine("Spec2");
+            for (int i = 0; i < dict["sp2"].Count; i++)
+            {
+                Console.WriteLine(dict["sp2"][i].Id);
+            }
+            Console.WriteLine(" ----------------------------- ");
+            Console.WriteLine("Intermediate");
+
+            for (int i = 0; i < intermediate.Count; i++)
+            {
+                Console.WriteLine(intermediate[i].Id);
+            }
+
+            Console.WriteLine(" ----------------------------- ");
+            Console.WriteLine("Spec3");
+            for (int i = 0; i < dict["sp3"].Count; i++)
+            {
+                Console.WriteLine(dict["sp3"][i].Id);
+            }
+
+
+            Console.WriteLine(" ----------------------------- ");
+            Console.WriteLine("Success");
+            for (int i = 0; i < success.Count; i++)
+            {
+                Console.WriteLine(success[i]);
             }
 
 
 
-
-
-
-
-
-
-            //for (int i = 0; i < dict["sp1"].Count; i++)
-            //{
-            //    Console.WriteLine(dict["sp1"][i].Id);
-            //}
-
-
-
-            //List<List<Abiturient>> abiturients = new List<List<Abiturient>>() { new List<Abiturient>(2), new List<Abiturient>(2) };
-            //List<Abiturient> abInQueue = new List<Abiturient>();
-
-            //for (int i = 0; i < 2; i++)
-            //{
-            //    for (int j = 0; j < ab.Count; j++)
-            //    {
-
-            //        if (abiturients[ab[j].Specialities[i] - 1].Count < abiturients[ab[j].Specialities[i] - 1].Capacity)
-            //        {
-            //            abiturients[ab[j].Specialities[i] - 1].Add(ab[j]);
-            //            ab.Remove(ab[j]);
-            //            j--;
-            //        }
-            //        else
-            //        {
-            //            var minValueInList = abiturients[ab[j].Specialities[i] - 1].Min(a => a.Result); // 240
-            //            if (minValueInList < ab[j].Result)
-            //            {
-            //                Abiturient removeAbit = abiturients[ab[j].Specialities[i] - 1]
-            //                    .Where(a => a.Result < minValueInList).FirstOrDefault();
-            //                ab.Add(removeAbit);
-            //                abiturients[ab[j].Specialities[i] - 1].Remove(removeAbit);
-            //                abiturients[ab[j].Specialities[i] - 1].Add(ab[j]);
-
-            //            }
-            //            else
-            //            {
-            //                continue;
-            //            }
-            //        }
-            //    }
-            //}
-            //Console.WriteLine(ab[0].Id);
-            //Console.WriteLine("Абитуриенты 1 специальности: ");
-            //for (int i = 0; i < abiturients[0].Count; i++)
-            //{
-            //    Console.WriteLine(abiturients[0][i].Id.ToString());
-            //}
-            //Console.WriteLine("Абитуриенты 2 специальности: ");
-            //for (int i = 0; i < abiturients[1].Count; i++)
-            //{
-            //    Console.WriteLine(abiturients[1][i].Id.ToString());
-            //}
-            //Console.WriteLine("Абитуриенты в очереди: ");
-            //for (int i = 0; i < abInQueue.Count; i++)
-            //{
-            //    Console.WriteLine(abInQueue[i].Id.ToString());
-            //}
-            //Console.WriteLine(abiturients[ab[0].Specialities[0] - 1]
-            //                    .Min(a => a.Result));
             Console.ReadKey();
         }
     }
